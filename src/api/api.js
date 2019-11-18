fee90ba7dee1eca5f2171fb23e921c23c0fec97c
h3n5 <295779204@qq.com>
2019-11-18 11:01:32 +0800

    github page test

--- src/api/api.js
+++ src/api/api.js
@@ -1,163 +1,163 @@
 import apiMusic from './api.url'
 import HttpRequest from './axios'
 const baseUrl =
-	process.env.NODE_ENV === 'development'
-		? 'http://34.80.55.4:3000'
-		: 'http://localhost:3000'
+  process.env.NODE_ENV === 'development'
+    ? 'http://34.80.55.4:3000'
+    : 'https://34.80.55.4:3000'
 let axios = new HttpRequest(baseUrl)
 export const getBanner = () => {
-	return axios.request({
-		url: apiMusic.banner,
-		method: 'get'
-	})
+  return axios.request({
+    url: apiMusic.banner,
+    method: 'get'
+  })
 }
 export const getpersonalized = () => {
-	return axios.request({
-		url: apiMusic.personalized,
-		method: 'get'
-	})
+  return axios.request({
+    url: apiMusic.personalized,
+    method: 'get'
+  })
 }
 export const getSongListByOrder = data => {
-	return axios.request({
-		url: apiMusic.topPlaylist,
-		method: 'get',
-		params: data
-	})
+  return axios.request({
+    url: apiMusic.topPlaylist,
+    method: 'get',
+    params: data
+  })
 }
 export const getAllTag = (params = {}) => {
-	return axios.request({
-		url: apiMusic.allTag,
-		method: 'get',
-		params: params
-	})
+  return axios.request({
+    url: apiMusic.allTag,
+    method: 'get',
+    params: params
+  })
 }
 export const getSongUrl = id => {
-	return axios.request({
-		url: apiMusic.musicUrl,
-		method: 'get',
-		params: {
-			id: id
-		}
-	})
+  return axios.request({
+    url: apiMusic.musicUrl,
+    method: 'get',
+    params: {
+      id: id
+    }
+  })
 }
 export const getLyric = id => {
-	return axios.request({
-		url: apiMusic.lyric,
-		method: 'get',
-		params: {
-			id: id
-		}
-	})
+  return axios.request({
+    url: apiMusic.lyric,
+    method: 'get',
+    params: {
+      id: id
+    }
+  })
 }
 export const gethotSearch = () => {
-	return axios.request({
-		url: apiMusic.hotSearch,
-		method: 'get'
-	})
+  return axios.request({
+    url: apiMusic.hotSearch,
+    method: 'get'
+  })
 }
 export const searchMusic = data => {
-	return axios.request({
-		url: apiMusic.search,
-		method: 'get',
-		params: {
-			keywords: data
-		}
-	})
+  return axios.request({
+    url: apiMusic.search,
+    method: 'get',
+    params: {
+      keywords: data
+    }
+  })
 }
 export const getsongDetail = id => {
-	return axios.request({
-		url: apiMusic.songDetail,
-		method: 'get',
-		params: {
-			ids: id
-		}
-	})
+  return axios.request({
+    url: apiMusic.songDetail,
+    method: 'get',
+    params: {
+      ids: id
+    }
+  })
 }
 export const getsongAlbum = id => {
-	return axios.request({
-		url: apiMusic.album,
-		method: 'get',
-		params: {
-			id: id
-		}
-	})
+  return axios.request({
+    url: apiMusic.album,
+    method: 'get',
+    params: {
+      id: id
+    }
+  })
 }
 export const loginCellphone = (
-	params = {
-		phone: '',
-		password: ''
-	}
+  params = {
+    phone: '',
+    password: ''
+  }
 ) => {
-	return axios.request({
-		url: apiMusic.loginCellphone,
-		method: 'get',
-		params: params
-	})
+  return axios.request({
+    url: apiMusic.loginCellphone,
+    method: 'get',
+    params: params
+  })
 }
 export const loginEmail = (
-	params = {
-		email: '',
-		password: ''
-	}
+  params = {
+    email: '',
+    password: ''
+  }
 ) => {
-	return axios.request({
-		url: apiMusic.loginEmail,
-		method: 'get',
-		params: params
-	})
+  return axios.request({
+    url: apiMusic.loginEmail,
+    method: 'get',
+    params: params
+  })
 }
 export const logout = (params = {}) => {
-	return axios.request({
-		url: apiMusic.logout,
-		method: 'get',
-		params: params
-	})
+  return axios.request({
+    url: apiMusic.logout,
+    method: 'get',
+    params: params
+  })
 }
 export const userPlaylist = (
-	params = {
-		uid: ''
-	}
+  params = {
+    uid: ''
+  }
 ) => {
-	return axios.request({
-		url: apiMusic.userPlaylist,
-		method: 'get',
-		params: params
-	})
+  return axios.request({
+    url: apiMusic.userPlaylist,
+    method: 'get',
+    params: params
+  })
 }
 export const userRecord = (
-	params = {
-		uid: '',
-		type: 1
-	}
+  params = {
+    uid: '',
+    type: 1
+  }
 ) => {
-	return axios.request({
-		url: apiMusic.userRecord,
-		method: 'get',
-		params: params
-	})
+  return axios.request({
+    url: apiMusic.userRecord,
+    method: 'get',
+    params: params
+  })
 }
 export const playlistDetail = (
-	params = {
-		id: ''
-	}
+  params = {
+    id: ''
+  }
 ) => {
-	return axios.request({
-		url: apiMusic.playlistDetail,
-		method: 'get',
-		params: params
-	})
+  return axios.request({
+    url: apiMusic.playlistDetail,
+    method: 'get',
+    params: params
+  })
 }
 export const recommendSongs = (params = {}) => {
-	return axios.request({
-		url: apiMusic.recommendSongs,
-		method: 'get',
-		params: params
-	})
+  return axios.request({
+    url: apiMusic.recommendSongs,
+    method: 'get',
+    params: params
+  })
 }
 export const toplist = (params = {}) => {
-	return axios.request({
-		url: apiMusic.toplist,
-		method: 'get',
-		params: params
-	})
+  return axios.request({
+    url: apiMusic.toplist,
+    method: 'get',
+    params: params
+  })
 }
